a731da73c191f67f1b187717a8409bba38b8fa3e
2over12 <ian.smith@trailofbits.com>
2022-01-12 17:05:44 -0500

    added submod

--- flake.nix
+++ flake.nix
@@ -4,20 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
       flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = nixpkgs.legacyPackages.${system};
-        binary_type_inference = pkgs.rustPlatform.buildRustPackage rec {
-            pname = "binary_type_inference";
-            version = "0.1.0";
-            src = self;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-          };
+        naersk-lib = naersk.lib."${system}";
+        binary_type_inference = naersk-lib.buildPackage {
+        pname = "my-project";
+        root = ./.;
+      };
       in {
         defaultPackage = binary_type_inference;
         devShell = pkgs.mkShell { buildInputs = [ pkgs.cargo pkgs.rustc pkgs.souffle ]; };
