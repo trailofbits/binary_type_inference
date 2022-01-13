1d87b33d57b8ca3a2c499da011b4e1f51f76f1cf
2over12 <ian.smith@trailofbits.com>
2022-01-13 08:11:13 -0500

    added back building

--- flake.nix
+++ flake.nix
@@ -4,18 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
+  outputs = { self, nixpkgs, flake-utils }:
       flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = nixpkgs.legacyPackages.${system};
-        naersk-lib = naersk.lib."${system}";
-        binary_type_inference = naersk-lib.buildPackage {
-        pname = "my-project";
-        root = ./.;
-      };
+        binary_type_inference = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "binary_type_inference";
+            version = "0.1.0";
+            src = self;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+          };
       in {
         defaultPackage = binary_type_inference;
         devShell = pkgs.mkShell { buildInputs = [ pkgs.cargo pkgs.rustc pkgs.souffle ]; };
