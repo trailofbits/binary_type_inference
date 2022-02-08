eae9406a906f9bebc6c1cccbf240931fd1570327
2over12 <ian.smith@trailofbits.com>
2022-02-08 13:14:51 -0500

    added additional field

--- test_data/big_list_test.c
+++ test_data/big_list_test.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 struct LL {
   struct LL* next;
   int handle;
+  char* name;
 };
+
+int print_name(struct LL* list) { return puts(list->name); }
 
 int close_last(struct LL* list) {
   while (list->next != NULL) {
@@ -16,6 +20,8 @@ int top_level(struct LL* list1, struct LL* list2) {
   int res = close_last(list1);
   if (res >= 0) {
     return close_last(list2);
+  } else {
+    print_name(list1);
   }
 
   return res;
