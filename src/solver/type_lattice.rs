2be97ebc05f2063867eb3eb284ce6c634a85080d
2over12 <ian.smith@trailofbits.com>
2022-03-21 16:41:20 -0400

    removed debug prints and fixed lattice semantics wrt to bounds

--- src/solver/type_lattice.rs
+++ src/solver/type_lattice.rs
@@ -227,7 +227,6 @@ impl LatticeDefinition {
         let meet = Rc::new(self.create_meet_table());
         let lt_set = Rc::new(self.create_less_than_sets());
 
-        println!("{:?}", meet);
         let top = CustomLatticeElement {
             elem: self.top_handle.clone(),
             bot: self.bottom_handle.clone(),
