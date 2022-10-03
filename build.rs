bb4a138c37fbb2b0afd369f89610ab407c670a6a
2over12 <ian.smith@trailofbits.com>
2022-10-03 11:20:24 -0400

    add ctype debug info

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 use std::io::Result;
 fn main() -> Result<()> {
-    prost_build::compile_protos(
+    let mut config = prost_build::Config::new();
+    // TODO(Ian): This doesnt actually guarentee correct naming per the issue but at least useful for debugging
+    config.type_attribute(".", "#[derive(serde::Serialize)]");
+    config.type_attribute(".", "#[serde(rename_all = \"camelCase\")]");
+    config.compile_protos(
         &[
             "data_formats/ctypes.proto",
             "data_formats/constraints.proto",
